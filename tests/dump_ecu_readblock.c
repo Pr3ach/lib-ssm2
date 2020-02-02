00b87ac4cfd8ba88985d4b32486f06077f6fc6d4
Preacher <Pr3acher@protonmail.com>
2020-02-02 12:30:10 +0100

    tests: new tests for dump using both methods; archive of older dumps

---
+++ tests/dump_ecu_readblock.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include "../src/ssm2.h"
+
+int main(void)
+{
+	unsigned int pos = 0;
+	unsigned int i  = 0;
+	unsigned char buf[0x80] = {0};
+	int ret = 0;
+
+	if (ssm2_open("/dev/ttyUSB0") != SSM2_ESUCCESS)
+	{
+		perror("[!] SSM2 open fail");
+		exit(-1);
+	}
+
+	for (pos = 0x00; pos < 0xffff7f; pos += 0x80)
+	{
+		if ((ret = ssm2_ecu_readblock(pos, 0x80, buf)) != SSM2_ESUCCESS)
+		{
+			printf("Error: %s\n", ssm2_strerror(ret));
+			ssm2_close();
+			exit(-1);
+		}
+
+		for (i = 0; i < 0x80; i++)
+			printf("%c", buf[i]);
+	}
+	ssm2_close();
+
+	return 0;
+}
+
